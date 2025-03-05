c5d1026ca31cefc6ddac8d2a099770262fd33146
Tejodeep Mitra Roy <tejodeepmitraroy2002@gmail.com>
2025-03-06 02:21:05 +0530

    feat: update the images

--- src/components/CallToActionButton.tsx
+++ src/components/CallToActionButton.tsx
@@ -5,7 +5,7 @@ import { Button } from "./ui/button";
 
 export const CallToActionButton = () => {
   return (
-    <Button className="gap-5 rounded-2xl border border-secondary bg-gradient-to-r from-primary to-secondary px-5 py-6 font-space-grotesk text-lg">
+    <Button className="gap-5 rounded-2xl border border-secondary bg-linear-to-r from-primary text-white to-secondary px-5 py-6 font-space-grotesk text-lg">
       Book A Friendly call{" "}
       <div className="flex items-center justify-center rounded-md border border-white p-2">
         <CiLocationArrow1 className="fill-white text-lg" />
@@ -16,7 +16,7 @@ export const CallToActionButton = () => {
 
 export const ContactUsButton = () => {
   return (
-    <Button className="bg-linear-to-r gap-5 rounded-2xl border border-secondary  from-primary to-secondary font-space-grotesk text-base">
+    <Button className="bg-linear-to-r gap-5 rounded-2xl border border-secondary  from-primary text-white to-secondary font-space-grotesk text-base">
       <Phone size={24} />
       Contact US
     </Button>
